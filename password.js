659cc0c414a3205ec8048b6fec614001c8094c60
frejlarsen <frejnohrlarsen@yahoo.com>
2021-10-27 20:44:15 +0200

    Update password.js

--- password.js
+++ password.js
@@ -1,13 +1,32 @@
 function boop() {
 
-	if (document.getElementById("number").value==123) {
-		text = "That's the correct number!"
-	} else {
-		text = "Sorry, that's a wrong number"
-	}
 
+	let num = document.getElementById("number").value;
+
+	if (num==123) {
+		text = "Yaaaay! Magisk tal!!!"
+	} else {
+
+		incorrect = num % 4;
+
+		switch(incorrect) {
+			case 0:
+				text = "Niks! Det nummer virkede ikke!"
+				break;
+			case 1:
+				text = "Ærgeligt! Prøv igen!"
+				break;
+			case 2:
+				text = "Beklager, det tal er ikke magisk :("
+				break;
+			case 3:
+				text = "Hov, det var vist en smutter!"
+				break;
+			default:
+				text = "Ej altså, det virkede ikke!"
+		}
+	}
 
 	var clueDiv = document.getElementById("clue")
 	clueDiv.innerHTML = "<p>" + text + "!</p>"
-
 }
